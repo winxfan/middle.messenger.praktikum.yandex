0106d4723bdf121f3879d4c1280c7ceadcc12e76
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2022-12-03 20:15:19 +0300

    fix types and tsconfig.json

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,23 +1,11 @@
 import Block from "./Block";
 
 export function renderElement(rootNode: string | null | undefined, component: Block) {
-  if(!rootNode) {
-    return
+  if(!rootNode || !component) {
+    throw new Error("rootNode или component не найден");
   }
 
   const root: HTMLElement | null = document.querySelector(rootNode)
-
-  // let template = PAGES['login']
-  //
-  // if (!template) {
-  //   template = PAGES['login']
-  // }
-  //
-  // const html = template()
-  //
-  // if (app !== null) {
-  //   app.innerHTML = html
-  // }
 
   if (!root) {
     throw new Error("Корневой элемент не найден");
